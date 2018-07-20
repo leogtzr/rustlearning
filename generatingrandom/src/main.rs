90a65596632119314ef629dad97a81e3d01e4141
leogtzr <leogutierrezramirez@gmail.com>
2018-07-19 22:57:29 -0600

    Generating random numbers

---
+++ generatingrandom/src/main.rs
@@ -0,0 +1,15 @@
+extern crate rand;
+
+use rand::distributions::{IndependentSample, Range};
+
+fn main() {
+    let between = Range::new(1, 100);
+    let mut rng = rand::thread_rng();
+
+    let total = 10;
+
+    for _ in 0..total {
+    	let a = between.ind_sample(&mut rng);
+    	println!("Val: {}", a);
+    }
+}
