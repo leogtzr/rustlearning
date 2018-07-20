13c128e79c4d02559fb9ab40b3091f7d4e653e92
leogtzr <leogutierrezramirez@gmail.com>
2018-07-19 23:28:45 -0600

    More code for guessing the game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,27 +8,39 @@ use rand::Rng;
 fn main() {
 	
 	println!("Guess the number!");
-	println!("Please input your guess.");
+
+	loop {
+		println!("Please input your guess.");
 	
-	let secret_number = rand::thread_rng().gen_range(1, 100);
-	println!("The secret number is: {}", secret_number);
+		let secret_number = rand::thread_rng().gen_range(1, 100);
+		println!("The secret number is: {}", secret_number);
 
-	// :: an associated function ... 
-	// Some languages call this static methods ... 
-	let mut guess = String::new();
+		// :: an associated function ... 
+		// Some languages call this static methods ... 
+		let mut guess = String::new();
 
-	io::stdin().read_line(&mut guess).expect("Failed to read line");
+		io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-	// trim() eliminates the new line.
-	let guess: u32 = guess.trim().parse()
-		.expect("Please type a number!");
-	//io::stdin().read_line(&mut guess).expect("Failed to read line");
-	println!("You guessed: {}", guess);
+		// trim() eliminates the new line.
+		// :type -> we are anotating the variable with a type.
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("Enter a valid number! :@");
+				continue;
+			}
+		};
+		//io::stdin().read_line(&mut guess).expect("Failed to read line");
+		println!("You guessed: {}", guess);
 
-	match guess.cmp(&secret_number) {
-		Ordering::Less => println!("Too small!"),
-		Ordering::Greater => println!("Too big!"),
-		Ordering::Equal => println!("You win!"),
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => {
+				println!("You win!");
+				break;
+			}
+		}
 	}
 
 }
