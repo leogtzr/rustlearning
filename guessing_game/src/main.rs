059229103eec44631e53e01b9de3f2056f141aa7
leogtzr <leogutierrezramirez@gmail.com>
2018-07-20 20:44:30 -0600

    Fixing guessing game.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,11 +8,11 @@ use rand::Rng;
 fn main() {
 	
 	println!("Guess the number!");
+	let secret_number = rand::thread_rng().gen_range(1, 100);
 
 	loop {
 		println!("Please input your guess.");
 	
-		let secret_number = rand::thread_rng().gen_range(1, 100);
 		println!("The secret number is: {}", secret_number);
 
 		// :: an associated function ... 
