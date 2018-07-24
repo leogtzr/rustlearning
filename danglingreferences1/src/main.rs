ffe2666284822c6842f70f8473c96377fef97c28
leogtzr <leogutierrezramirez@gmail.com>
2018-07-24 09:25:37 +0000

    Adding code explaining dangling references.

--- danglingreferences1/src/main.rs
+++ danglingreferences1/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
+	// We may be referencing a location in memory that may have given to someone else, by freeing some memory
+	// while preserving a pointer to that memory.
+
+	// The way to fix this code is with lifetimes.	
     let reference_to_nothing = dangle();
 }
 
 // The following code fails at compile time:
-fn dangle() -> &String {
+fn dangle() -> &String {				// Returns a reference to a String.
 	let s: String = "Holis";
 	&s
-}
+}						// Here, "s" goes out of scope.
+						// Danger and dangling!
