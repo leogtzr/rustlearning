ab1bf2589409e496b01d414a28ca1959f6c928b9
leogtzr <leogutierrezramirez@gmail.com>
2018-07-28 13:25:55 -0600

    Adding code about patterns that bind to values.

---
+++ patterns_that_bind_to_values/src/main.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+enum UsState {
+	Alabama,
+	Alaska,
+}
+
+#[derive(Debug)]
+enum Coin {
+	Penny,
+	Nickel,
+	Dime,
+	Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+	match coin {
+		Coin::Penny => 1,
+		Coin::Nickel => 5,
+		Coin::Dime => 10,
+		Coin::Quarter(state) => {
+			println!("State quarter from {:?}", state);
+			25
+		},
+	}
+}
+
+fn main() {
+	let x: u32 = value_in_cents(Coin::Quarter(UsState::Alaska));
+	println!("{:?}", x);
+}
