4bf1620c190eb4d1b9588746edd49918171d24aa
leogtzr <leogutierrezramirez@gmail.com>
2018-07-29 13:08:15 -0600

    Adding code about the _ placeholder.

---
+++ _placeholder/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+	let some_u8_value: u8 = 1u8;
+
+	match some_u8_value {
+		1 => println!("Uno"),
+		2 => println!("Dos"),
+		_ => {
+			println!("Cualquier otro valor ... ");
+		}
+	}
+}
