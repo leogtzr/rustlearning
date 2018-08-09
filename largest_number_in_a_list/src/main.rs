d7d3b6ce92bd158c39b0595b851a35e5cea86779
leogtzr <leogutierrezramirez@gmail.com>
2018-08-08 21:52:02 -0600

    Largest number in a list - function.

--- largest_number_in_a_list/src/main.rs
+++ largest_number_in_a_list/src/main.rs
@@ -1,12 +1,19 @@
+fn largest(lst: &Vec<i32>) -> i32 {
+	let mut largest = lst[0];
+	for &number in lst {
+		if number > largest {
+			largest = number;
+		}
+	}
+	largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let mut largest = number_list[0];
+    let result = largest(&number_list);
+    println!("The largest number is: {}", result);
 
-    for number in number_list {
-    	if number > largest {
-    		largest = number;
-    	}
-    }
-
-    println!("The largest number is: {}", largest);
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+	println!("The largest number is: {}", result);    
 }
