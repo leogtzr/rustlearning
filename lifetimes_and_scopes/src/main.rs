b6801bb3e9783b9c768667f425981bac3477c9d6
leogtzr <leogutierrezramirez@gmail.com>
2018-08-11 11:49:20 -0600

    Explanation and code about lifetimes and different scopes.

--- lifetimes_and_scopes/src/main.rs
+++ lifetimes_and_scopes/src/main.rs
@@ -18,9 +18,13 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is: {}", result);
+	
+	// string1 and string2 now have different lifetimes. This code won't compile.
+	let string1 = String::from("long string is long");
+	let result;
+	{
+		let string2 = String::from("abcd");
+		result = longest(string1.as_str(), string2.as_str());
+		println!("{}", result);
+	}
 }
