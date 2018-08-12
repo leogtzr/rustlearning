9d3b46bae9560759701280ef259f1e1c0f211dc3
leogtzr <leogutierrezramirez@gmail.com>
2018-08-12 15:06:06 -0600

    More code about lifetime elision.

--- lifetime_annotations_in_struct_definitions/src/main.rs
+++ lifetime_annotations_in_struct_definitions/src/main.rs
@@ -3,6 +3,21 @@ struct ImportantExcerpt<'a> {
 	part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+	fn level(&self) -> i32 {
+		3
+	}
+}
+
+impl<'a> ImportantExcerpt<'a> {
+	fn announce_and_return_part(&self, announcement: &str) -> &str {
+		println!("Attention please: {}", announcement);
+		self.part    				// the return lifetime is not specified
+									// but based on the rules self's elision
+									// lifetime is assigned here.
+	}
+}
+
 fn main() {
     let novel = String::from("Call me Leo. Some years ago ...");
     let first_sentence = novel.
