3707cb4848d291329399b4e5d701c185e1bae68b
leogtzr <leogutierrezramirez@gmail.com>
2018-08-18 16:55:23 -0600

    Using non-panic error handling.

--- reading_a_file/src/main.rs
+++ reading_a_file/src/main.rs
@@ -1,31 +1,36 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 struct Config {
 	query: String,
 	filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-	let query = args[1].clone();
-	let filename = args[2].clone();
-	Config {query, filename}
-}
+// fn parse_config(args: &[String]) -> Config {
+// 	let query = args[1].clone();
+// 	let filename = args[2].clone();
+// 	Config {query, filename}
+// }
 
 impl Config {
-	fn new(args: &[String]) -> Config {
+	fn new(args: &[String]) -> Result<Config, &'static str> {
+		if args.len() < 3 {
+			return Err("not enough arguments.");
+		}
+
 		let query = args[1].clone();
 		let filename = args[2].clone();
-		Config {query, filename}
+		Ok(Config {query, filename})
 	}
 }
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    // let query = &args[1];
+    // let filename = &args[2];
 
     // println!("{:?} - [{}] - [{}]", args, query, filename);
 
@@ -37,9 +42,14 @@ fn main() {
 
     //let (query, filename) = parse_config(&args);
     //let config = parse_config(&args);
-    let config = Config::new(&args);
-    println!("Searching for: {:?}", config.query);
-    println!("In file: {}", config.filename);
+    // let config = Config::new(&args);
+    // println!("Searching for: {:?}", config.query);
+    // println!("In file: {}", config.filename);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+    	println!("Problem parsing arguments: {}", err);
+		process::exit(1);
+    });
 
     let mut f = File::open(config.filename).expect("file not found ... ");
 }
