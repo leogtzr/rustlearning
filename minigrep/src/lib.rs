cafeb7c2ab611e981cca348f727058a9c2eb7cf1
leogtzr <leogutierrezramirez@gmail.com>
2018-08-25 17:23:53 -0600

    Code to search for insensitive case.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -5,6 +5,7 @@ use std::error::Error;
 pub struct Config {
 	pub query: String,
 	pub filename: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
@@ -15,7 +16,7 @@ impl Config {
 
 		let query = args[1].clone();
 		let filename = args[2].clone();
-		Ok(Config {query, filename})
+		Ok(Config {query, filename, case_sensitive: true})
 	}
 }
 
@@ -24,7 +25,12 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 	let mut contents = String::new();
 	f.read_to_string(&mut contents)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results { 
         println!("{}", line);
     }
     
